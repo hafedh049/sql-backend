f6925db30fcec8c59ba5e3798096bd163cf58be2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-03-06 09:45:03 +0100

    Modified  .gitignore M  lib/main.dart A  lib/models/products_model.dart A  lib/utils/callbacks.dart A  lib/utils/shared.dart M  pubspec.yaml M  test/widget_test.dart

---
+++ lib/utils/shared.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color scaffoldColor = Color.fromARGB(255, 240, 240, 240);
+const Color purpleColor = Color.fromARGB(255, 124, 120, 239);
+const Color darkColor = Color.fromARGB(255, 250, 250, 250);
+const Color blackColor = Color.fromARGB(255, 31, 31, 31);
+const Color greenColor = Colors.greenAccent;
+const Color blueColor = Colors.blueAccent;
+const Color redColor = Colors.red;
+const Color transparentColor = Colors.transparent;
